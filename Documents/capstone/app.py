952b5a18a33cb0bcd9d2bfd1cca7568ac9e80759
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2024-05-31 20:31:10 +0700

    feat(doc api): add swagger for doc api

--- Documents/capstone/app.py
+++ Documents/capstone/app.py
@@ -1,10 +1,24 @@
 from flask import Flask;
 from dotenv import load_dotenv;
 from firestore import add_data
+from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'  
+API_URL = "/swagger/v1.json"   
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  
+        'app_name': "Test application"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 @app.route("/")
 def index_page():
